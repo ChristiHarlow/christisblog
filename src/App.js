13d2e95da397e7e4cd7f8ef14c8102913cb718f2
ChristiHarlow <cah120671@gmail.com>
2024-10-11 01:03:11 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import About from './components/About';
+import BlogPosts from './components/BlogPosts';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/blog" element={<BlogPosts />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
