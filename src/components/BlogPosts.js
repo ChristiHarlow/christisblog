a8bcd5eeaa6ddd209666b31b72910524ed4e129b
ChristiHarlow <cah120671@gmail.com>
2024-10-11 03:50:07 -0400

    Adding
    Adversity

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,4 +1,26 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+
+// Define blogPosts array
+const blogPosts = [
+  {
+    id: 1,
+    title: 'Healing and Growth Through Adversity',
+    content: 'This post explores how my experiences have led to personal growth and healing...',
+    category: 'Personal Growth'
+  },
+  {
+    id: 2,
+    title: 'Touched by Racism and Discrimination',
+    content: 'In 2018, I faced an experience that changed my perspective...',
+    category: 'Racism & Discrimination'
+  },
+  {
+    id: 3,
+    title: 'New Beginnings',
+    content: 'After years of working in different fields, I decided to make a career change...',
+    category: 'Other Life Experiences'
+  }
+];
 
 function BlogPosts() {
   const categories = [...new Set(blogPosts.map(post => post.category))];
@@ -13,8 +35,7 @@ function BlogPosts() {
             .map(filteredPost => (
               <div key={filteredPost.id}>
                 <h3>{filteredPost.title}</h3>
-                <p>{filteredPost.content.substring(0, 100)}...</p> {/* Limit content length */}
-                <Link to={`/blog/${filteredPost.id}`}>Read More</Link>
+                <p>{filteredPost.content}</p>
               </div>
             ))}
         </div>
@@ -24,4 +45,5 @@ function BlogPosts() {
 }
 
 export default BlogPosts;
+
 
