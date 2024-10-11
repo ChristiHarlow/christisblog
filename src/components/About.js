2bccf3dac9199f50a33bccaa8be894a4eacd1230
ChristiHarlow <cah120671@gmail.com>
2024-10-11 01:30:44 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './About.css'; // Importing the CSS for styling
 
 function About() {
   return (
-    <div>
-      
-      <p>
+    <div className="about-container">
+      <h1 className="about-title">About Christi's Blog</h1>
+      <p className="about-content">
         Welcome to my blog! I’m Christi, and here I share my journey of healing,
-        growth, and life experiences. You'll find posts on topics like racism and 
-        discrimination, personal development, and many other subjects that have shaped 
-        my life. I hope to create a space for open dialogue and reflection.
+        growth, and life experiences. You’ll find posts on topics like racism and 
+        discrimination, personal development, and many other subjects that have shaped
+        my life. I hope to create a space for open dialogue and reflection. 
       </p>
     </div>
   );
 }
 
 export default About;
+
