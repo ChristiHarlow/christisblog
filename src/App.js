d7cfd9ffb1e98f611023ce8b58783ca845c1d537
ChristiHarlow <cah120671@gmail.com>
2024-10-11 01:09:06 -0400

    Additions
    Adversity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import About from './components/About';
+import Adversity from './components/Adversity';
 import BlogPosts from './components/BlogPosts';
 import Header from './components/Header';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/blog" element={<BlogPosts />} />
+          <Route path="/blog/adversity" element={<Adversity />} /> {/* Route for Adversity.js */}
         </Routes>
       </div>
     </Router>
---
+++ src/components/Adversity.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Adversity() {
+  return (
+    <div>
+      <h1>Healing and Growth Through Adversity</h1>
+      <p>
+        This post explores how my experiences have led to personal growth and healing...
+        (Add more content here as you like)
+      </p>
+    </div>
+  );
+}
+
+export default Adversity;
+
