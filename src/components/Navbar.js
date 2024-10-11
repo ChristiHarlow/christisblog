343c3e5762cb1f98fdf08ebbbece68b733a5e963
ChristiHarlow <cah120671@gmail.com>
2024-10-11 14:27:38 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'; // Ensure the path is correct
+import '../styles/Navbar.css'; // Ensure the path to the CSS is correct
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/adversity">Adversity</Link></li>
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li><Link to="/about" className="navbar-link">About</Link></li>
+        <li><Link to="/blog" className="navbar-link">Blog</Link></li>
+        <li><Link to="/adversity" className="navbar-link">Adversity</Link></li>
       </ul>
     </nav>
   );
