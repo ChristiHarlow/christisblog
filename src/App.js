eac656d52dd9131b6610e342a78be17744d8a4cd
ChristiHarlow <cah120671@gmail.com>
2024-10-11 02:23:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
 import About from './components/About';
 import Adversity from './components/Adversity';
 import BlogPosts from './components/BlogPosts';
