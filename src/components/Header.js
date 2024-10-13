c9dc6cec9a638999fba78d5314952c0142d7532c
ChristiHarlow <cah120671@gmail.com>
2024-10-13 18:14:57 -0400

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import '../styles/Header.css'; // Ensure the path to CSS is correct
+import '../styles/Header.css'; // Make sure your styles are correctly imported
 
 function Header() {
   return (
     <header className="header">
-      {/* Correct path to your header image */}
       <div className="header-banner-container">
         <img src={process.env.PUBLIC_URL + '/images/header-banner.png'} alt="Banner" className="header-banner" />
-        {/* Navbar */}
         <nav className="navbar">
           <a href="/about" className="nav-link">About</a>
           <div className="dropdown">
@@ -25,6 +23,7 @@ function Header() {
 }
 
 export default Header;
+
 
 
 
