bee43a9799e8415d00544c8d39ab4621794825e1
ChristiHarlow <cah120671@gmail.com>
2024-10-13 18:28:29 -0400

    Navbar updates

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Adversity from './components/Adversity';
 import BlogPosts from './components/BlogPosts';
 import Footer from './components/Footer'; // Import Footer if added
 import Header from './components/Header';
-//import Navbar from './components/Navbar'; // Import Navbar if added
 
 function App() {
   return (
     <Router>
-      <Header /> {/* Display header */}
-      <Navbar /> {/* Display navbar for navigation */}
+      <Header /> {/* Header includes the navbar */}
       <div className="App">
         <Routes>
           <Route path="/about" element={<About />} />
@@ -18,12 +16,13 @@ function App() {
           <Route path="/blog" element={<BlogPosts />} />
         </Routes>
       </div>
-      <Footer /> {/* Make sure the footer is here */}
+      <Footer /> {/* Footer remains intact */}
     </Router>
   );
 }
 
 export default App;
+
 
 
 
