f58ec35914ca8ab2d57a945e3c54ab59d6554832
ChristiHarlow <cah120671@gmail.com>
2024-10-13 17:19:33 -0400

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import '../styles/Header.css'; // Ensure the path is correct
+import '../styles/Header.css'; // Make sure your styles are correctly imported
 
 function Header() {
   return (
     <header className="header">
-     <div className="header-art-container">
-        <img src="/images/canva-header.png" alt="Blog Art" className="header-art" />
+      <div className="header-content">
+        {/* Header Image */}
+        <div className="header-art-container">
+          <img src="/images/canva-header.png" alt="Blog Art" className="header-art" />
+        </div>
+        {/* Navbar */}
+        <nav className="navbar">
+          <a href="/about" className="nav-link">About</a>
+          <a href="/team" className="nav-link">Team</a>
+          <a href="/home" className="nav-link">Home</a>
+          <button className="menu-button">☰</button> {/* Optional menu button */}
+        </nav>
       </div>
     </header>
   );
