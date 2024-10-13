37b6888938f88b50fee9f00816de6bf8a4237e06
ChristiHarlow <cah120671@gmail.com>
2024-10-13 17:01:47 -0400

    Font change

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,9 @@ import '../styles/Header.css'; // Ensure the path is correct
 function Header() {
   return (
     <header className="header">
-    <h1 className="header-title">Christi's Blog</h1> {/* Keep it simple */}
      <div className="header-art-container">
         <img src="/images/canva-header.png" alt="Blog Art" className="header-art" />
       </div>
-    <p className="header-tagline">Sharing experiences on healing, growth, and life challenges</p>
     </header>
   );
 }
