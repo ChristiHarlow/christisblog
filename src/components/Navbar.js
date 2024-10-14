9076cd38c1b0c7586cf011d282229785268bf330
ChristiHarlow <cah120671@gmail.com>
2024-10-14 00:20:07 -0400

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'; // Ensure the path to the CSS is correct
 
 function Navbar() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
-
   return (
     <nav className="navbar">
       <ul className="navbar-list">
         <li><Link to="/about" className="nav-link">About</Link></li>
         
         {/* Posts with dropdown for Adversity */}
-        <li className="dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+        <li className="dropdown">
           <button className="nav-link dropdown-button">Posts</button>
-          {dropdownOpen && (
-            <div className="dropdown-menu">
-              <Link to="/posts/adversity" className="dropdown-item">Adversity</Link>
-              {/* Add more blog post links here as needed */}
-            </div>
-          )}
+          <div className="dropdown-menu">
+            <Link to="/posts/adversity" className="dropdown-item">Adversity</Link>
+            {/* Add more blog post links here as needed */}
+          </div>
         </li>
 
         <li><Link to="/blogs" className="nav-link">Blogs</Link></li>
@@ -33,4 +25,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
 
