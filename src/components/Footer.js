64c5cfcd716a48015e9b8e999474e4b5b995f4a8
ChristiHarlow <cah120671@gmail.com>
2024-10-13 23:56:42 -0400

    Add footer banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,15 @@ import '../styles/Footer.css'; // Ensure the path is correct
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p className="footer-tagline">© 2024 Christi's Blog</p>
+      <div className="footer-banner-container">
+        <img src="/images/footer-banner.png" alt="Footer Banner" className="footer-banner" />
       </div>
+      <p className="footer-tagline">© 2024 Christi's Blog</p>
     </footer>
   );
 }
 
 export default Footer;
+
 
 
