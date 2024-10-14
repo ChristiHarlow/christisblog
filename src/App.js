0bbfb792562f161a1aebbc8e9a092277c6fe596e
ChristiHarlow <cah120671@gmail.com>
2024-10-13 20:40:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import BlogPosts from './BlogPosts';
-import Footer from './Footer';
-import Header from './Header';
-import Navbar from './Navbar';
+import About from './components/About'; // Ensure this is imported
+import BlogPosts from './components/BlogPosts';
+import Contact from './components/Contact'; // Ensure this is imported
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Navbar /> {/* Navbar is now between the header and footer */}
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/blogs/adversity" element={<BlogPosts />} />
@@ -21,6 +23,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
