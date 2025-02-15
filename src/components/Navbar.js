c2c47e4d3b659462e24e45d68f9807e6f0be8a3f
Christi Harlow <cah120671@gmail.com>
2025-02-14 22:41:36 -0500

    Css Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Import NavLink
 import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li><Link to="/about" className="nav-link">About</Link></li>
-        <li><Link to="/blogs" className="nav-link">Blogs</Link></li> {/* No more Posts */}
-        <li><Link to="/contact" className="nav-link">Contact</Link></li>
+        <li>
+          <NavLink to="/about" className="nav-link">  {/* Use NavLink */}
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/blogs" className="nav-link">  {/* Use NavLink */}
+            Blogs
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className="nav-link"> {/* Use NavLink */}
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
