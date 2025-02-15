90b75233f15dcbe5353e350ec56ea7eea2c00111
Christi Harlow <cah120671@gmail.com>
2025-02-15 01:11:02 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,19 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../styles/Contact.css';
-  
+
+function Contact() {  // Added the opening curly brace
   return (
     <div className="contact-container">
-      <div className="contact-info"> {/* Container for contact info */}
+      <div className="contact-info"> 
         <h2 className="contact-title">Contact Me</h2>
         <a href="mailto:christiaharlow@outlook.com" className="contact-link">
           <FontAwesomeIcon icon={faEnvelope} />
           Email Me
         </a>
       </div>
-
-      
     </div>
   );
-
+}
 
 export default Contact;
-
