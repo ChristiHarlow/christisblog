42a5feab797fe445cd28a05bbfe7ef8598424391
Christi Harlow <cah120671@gmail.com>
2025-02-14 22:45:55 -0500

    Update BlogPosts.js

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,38 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link, useParams } from 'react-router-dom'; // Import useParams
 import '../styles/BlogPosts.css';
-import blogPosts from './BlogPostDetails'; // Import your blog post data (from blogData.js)
+import blogPosts from './BlogPostDetails';
 
 function BlogPosts() {
+  const { slug } = useParams(); // Get the slug from the URL
   const categories = [...new Set(blogPosts.map(post => post.category))];
 
+  // Find the current post based on the slug (if there is one)
+  const currentPost = slug? blogPosts.find(post => post.slug === slug): null;
+
   return (
-    <div className="blog-posts-container"> {/* Updated container class name */}
-      <h1>Blog Posts</h1> {/* Main heading */}
+    <div className="blog-posts-container">
+      <h1>Blog Posts</h1>
 
-      {/* Links to specific posts (if needed) */}
-      <div className="post-links"> {/* Container for direct post links */}
-        <Link to="/posts/adversity" className="post-link">Adversity</Link>
-        <Link to="/posts/self-advocacy" className="post-link">Self-Advocacy</Link>
-      </div>
-
-      {categories.map(category => (
-        <div key={category}>
-          <h2>{category}</h2>
-          {blogPosts
-          .filter(post => post.category === category)
-          .map(post => ( // No need for filteredPost anymore
-              <div key={post.id} className="blog-post"> {/* Added blog-post class */}
-                <h3>{post.title}</h3>
-                <p>{post.content}</p> {/* Display full content here */}
-              </div>
-            ))}
+      {/* Conditionally render content */}
+      {currentPost? ( // If a slug is in the URL, show only that post
+        <div className="blog-post">
+          <h3>{currentPost.title}</h3>
+          <p>{currentPost.content}</p>
         </div>
-      ))}
+      ): ( // Otherwise, show all posts categorized
+        categories.map(category => (
+          <div key={category}>
+            <h2>{category}</h2>
+            {blogPosts
+            .filter(post => post.category === category)
+            .map(post => (
+                <div key={post.id} className="blog-post">
+                  <Link to={`/posts/${post.slug}`} className="post-title-link"> {/* Link to individual post */}
+                    <h3>{post.title}</h3>
+                  </Link>
+                  <p>{post.content.slice(0, 200)}...</p> {/* Shortened content */}
+                </div>
+              ))}
+          </div>
+        ))
+      )}
     </div>
   );
 }
 
 export default BlogPosts;
-
-
