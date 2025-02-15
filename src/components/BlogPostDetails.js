69c06a9029fb2db33231c3a20f96713dcd943fe3
Christi Harlow <cah120671@gmail.com>
2025-02-14 22:13:27 -0500

    Update BlogPostDetails.js

--- src/components/BlogPostDetails.js
+++ src/components/BlogPostDetails.js
@@ -1,4 +1,72 @@
+import React from 'react';
 
+import { useParams } from 'react-router-dom';
+
+
+
+const blogPosts = [
+
+{
+
+id: 1,
+
+title: 'Healing and Growth Through Adversity',
+
+content: 'Adversity often manifests in various forms, and recognizing it is the first step toward addressing its impact. ...',
+
+category: 'Personal Growth'
+
+},
+
+{
+
+id: 2,
+
+title: 'The Cost of Self-Advocacy: My Fight for Justice',
+
+content: 'Self-Advocacy is about standing up even when the odds are against you...',
+
+category: 'Racism & Discrimination'
+
+},Queries
+
+];
+
+
+
+function BlogPostDetails() {
+
+const { id } = useParams();
+
+const post = blogPosts.find(post => post.id === parseInt(id));
+
+
+
+if (!post) {
+
+return <h2>Post not found</h2>;
+
+}
+
+
+
+return (
+
+<div>
+
+<h1>{post.title}</h1>
+
+<p>{post.content}</p>
+
+</div>
+
+);
+
+}
+
+
+
+export default BlogPostDetails; 
 
 
 
