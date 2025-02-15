c5c26e314b273345ce5c273d5e75f6a7c63d892d
Christi Harlow <cah120671@gmail.com>
2025-02-14 22:56:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import About from "./components/About";
-// You don't need to import Adversity and SelfAdvocacy separately
-import BlogPosts from "./components/BlogPosts";
+import Adversity from "./components/Adversity";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import SelfAdvocacy from "./components/SelfAdvocacy";
 
 function App() {
   return (
@@ -16,9 +16,12 @@ function App() {
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/posts" element={<BlogPosts />} /> {/* For the blog post list */}
-        <Route path="/posts/:slug" element={<BlogPosts />} /> {/* For individual posts */}
-        {/* You don't need separate routes for Adversity and SelfAdvocacy */}
+        <Route path="/posts/adversity" element={<Adversity />} />
+        <Route
+          path="/posts/self-advocacy"
+          element={<SelfAdvocacy />}
+        />
+        {/* Remove the /posts and /posts/:slug routes */}
       </Routes>
       <Footer />
     </Router>
