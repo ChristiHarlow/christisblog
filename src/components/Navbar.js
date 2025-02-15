45b904079b5689ad68d4be0c9140de9383bb467b
Christi Harlow <cah120671@gmail.com>
2025-02-14 23:00:03 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li>
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
+        <li><Link to="/about" className="nav-link">About</Link></li>
+
+        <li className="dropdown">
+          <button className="nav-link dropdown-button">Blogs</button>
+          <div className="dropdown-menu">
+            <Link to="/posts/adversity" className="dropdown-item">Adversity</Link>
+            <Link to="/posts/self-advocacy" className="dropdown-item">Self-Advocacy</Link>
+          </div>
         </li>
-        <li>
-          <Link to="/posts/adversity" className="nav-link">
-            Adversity
-          </Link>
-        </li>
-        <li>
-          <Link to="/posts/self-advocacy" className="nav-link">
-            Self-Advocacy
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-        </li>
+
+        <li><Link to="/contact" className="nav-link">Contact</Link></li>
       </ul>
     </nav>
   );
